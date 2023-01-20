44b3a05fb71891add99793929b55c2b676fd0047
iman-ulanov <ulanoviman3@gmail.com>
2023-01-20 12:57:02 +0600

    added authorization

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Author
+from .serializers import AuthorRegisterSerializer
+
+
+class AuthorSerializer(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorRegisterSerializer
