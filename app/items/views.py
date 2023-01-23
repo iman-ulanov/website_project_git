ff52aeb092bb133e7d24979e86c068585fdc4978
DauletHuseinov <muslimovdamir50@gmail.com>
2023-01-23 10:25:08 +0300

    Niga

--- app/items/views.py
+++ app/items/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 from website_project_git.app.items.models import Item
 from website_project_git.app.items.serializers import ItemSerializer
 
 
 # Create your views here.
 
-class ItemViewSet(viewsets.ModelViewSet):
+class ItemViewSet(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
